3c9ae423c18c9a05b16929d33357ecabc729ef77
Rufus Pollock <rufus.pollock@datopian.com>
2021-05-04 23:22:04 +0200

    [all][xl]: standard nuxt setup ootb plus git headless cms with one sutra.
    * diamond sutra in markdown based on tnh translation
    * sutra page

---
+++ pages/sutras/_slug.vue
@@ -0,0 +1,26 @@
+<template>
+  <article class="prose lg:prose-xl max-w-2xl mx-auto pt-36">
+    <h1>{{ article.title }}</h1>
+    <p class="text-sm">
+      Source: <a :href="article.source"> {{article.source}}</a>
+      <br /><br />
+      Alternative sources: <a :href="article.alt"> {{article.alt}}</a>
+    </p>
+    <nuxt-content :document="article" />
+  </article>
+</template>
+
+<script>
+  export default {
+    async asyncData({ $content, params }) {
+      const article = await $content('sutras', params.slug).fetch()
+      return { article }
+    }
+  }
+</script>
+
+<style>
+body {
+  font-family: 'Libre Baskerville',  serif;
+}
+</style>
