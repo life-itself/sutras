b3c687b4c588c29826fe389cdf585b99cbfcae6f
Rufus Pollock <rufus.pollock@datopian.com>
2021-07-10 15:41:39 +0200

    [theme][s]: change favicon to scroll emoji.

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: "icon", type: "image/x-icon", href: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📜</text></svg>" },
       {
         rel: "stylesheet",
         href: "https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap"
