82822f254c988088f2b48ef8d1ecca2cf2cdc17d
rufuspollock <rufuspollock@users.noreply.github.com>
2021-07-14 17:43:13 +0000

    deploy: 36cb3c0eb9599d6282300b8540d03dbc9fd871a7

--- _nuxt/static/1626261131/sutras/diamond/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1626261131",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsutras\u002Fdiamond",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"48d28478"}}}}(null));
---
+++ _nuxt/static/1626284591/sutras/seneca-on-the-shortness-of-life/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1626284591",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsutras\u002Fseneca-on-the-shortness-of-life",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"97d62cdf"}}}}(null));
