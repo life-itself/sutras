219626428ac44e6fe9dbe5470739e25b5edd3316
rufuspollock <rufuspollock@users.noreply.github.com>
2021-07-20 06:41:34 +0000

    deploy: 2f902a6d8dad1db1421f5b2fd26bf9b357ebd744

--- _nuxt/static/1626469601/sutras/gateless-gate-mumonkan/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1626469601",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsutras\u002Fgateless-gate-mumonkan",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"69e85e7c"}}}}(null));
---
+++ _nuxt/static/1626763292/sutras/shodoka/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1626763292",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsutras\u002Fshodoka",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"bf5745f7"}}}}(null));
